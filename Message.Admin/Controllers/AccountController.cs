6bb7db68f6a394a8f72da3f9cb072d21646984cc
mykaggleteam <mykaggleteam@hotmail.com>
2020-11-16 20:01:50 +0300

    .
    .

---
+++ Message.Admin/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Message.Admin.Models;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Message.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("SignIn", "Account");
+        }
+    }
+}
